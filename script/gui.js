382fda0c7b44175d88208cb588f2c7a0266e33f1
Klara Ahlin <klaraahlin@Klaras-MacBook-Pro.local>
2015-03-12 12:35:28 +0100

    Added js to filtering

--- script/gui.js
+++ script/gui.js
@@ -1,7 +1,23 @@
 $(function() {
 
     $( ".filters" ).on( "click", ".filterbox", function() {
-  		$(this).toggleClass('active');
+    	$this = $(this);
+  		$this.toggleClass('active');
+  		$boxLabels = $('.box-label');
+  		$boxLabelBox = $('.filterCats');
+
+  		catStr = ' ';
+
+  		$this.parent().find('.active a').each(function( i ) {
+  			catStr = catStr + ' ' + this.text;
+  		});
+  		
+  		$boxLabelBox.html(catStr);
+
+
+
+  		
+
 	});
 
 });
