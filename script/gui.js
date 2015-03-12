558cdd14df6cff68aaa25f283ca3899101f10b2b
Klara Ahlin <klaraahlin@Klaras-MacBook-Pro.local>
2015-03-12 13:07:26 +0100

    filter script

--- script/gui.js
+++ script/gui.js
@@ -5,19 +5,48 @@ $(function() {
   		$this.toggleClass('active');
   		$boxLabels = $('.box-label');
   		$boxLabelBox = $('.filterCats');
+  		$category = $this.data('category');
+  		$boxSection = $('.box-section');
 
   		catStr = ' ';
+  		catList = new Array();
 
-  		$this.parent().find('.active a').each(function( i ) {
-  			catStr = catStr + ' ' + this.text;
+  		$this.parent().find('.active').each(function( i ) {
+  			catList.push($( this ).data( "category" ));
+  			catStr = catStr + ' ' + $( this ).find('a').text();
   		});
+
+
+
+  		$boxSection.each(function( i ) {
+  			
+  			$('.boxy-box').each(function() {
+  				$this = $(this);
+  				if ($this.data('category') == $category) {	
+  					$this.addClass('active');	
+
+  				}
+
+  				else {
+  					$this.addClass('cat-hide');
+  				}
+
+  				var i;
+				for (i = 0; i < catList.length; ++i) {
+					   console.log(catList[i]);
+				}
+
+  			});
+
+  		});
+
+
   		
   		$boxLabelBox.html(catStr);
 
 
 
-  		
-
 	});
 
 });
+
